f5a7d21678b80f586764895b4ae5097a4dc582ad
ls <e.nesterovich007@gmail.com>
2026-03-11 14:55:56 +0300

    javadoc 1.2

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"},{"l":"command"},{"l":"command.impl"},{"l":"exceptions"},{"l":"io"},{"l":"manager"},{"l":"model"},{"l":"model.movie"},{"l":"model.person"},{"l":"utils"},{"l":"validator"}]
